12fd347054501ee13cdbb7644e13a82b0f201485
Bilalisz <khan.tech885@gmail.com>
2021-06-10 21:07:50 +0500

    new work

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const mediaQuery = window.matchMedia( '( min-width: 768px )' )
+const mediaQuery = window.matchMedia("( min-width: 768px )");
 
 // Note the `matches` property
-if ( mediaQuery.matches ) {
-  console.log('Media Query Matched!','( min-width: 768px )')
+if (mediaQuery.matches) {
+  console.log("Media Query Matched!", "( min-width: 768px )");
 }
+
+const toogleBtn = () => {
+
+  const ul = document.querySelector(".nav-list");
+  ul.classList.add("trans")
+  ul.style.display = "block";
+
+};
