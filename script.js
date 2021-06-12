dd461b9da1ba40bb2e769b789ea88c166d5741d1
Bilal Khan <bilal.khan@argonteq.com>
2021-06-12 13:16:35 +0500

    'updates'

--- script.js
+++ script.js
@@ -6,9 +6,22 @@ if (mediaQuery.matches) {
 }
 
 const toogleBtn = () => {
-
   const ul = document.querySelector(".nav-list");
-  ul.classList.add("trans")
+  ul.classList.add("trans");
   ul.style.display = "block";
+  const toggleOpen = document.querySelector("#toggle-open");
+  const toggleClose = document.querySelector("#toggle-close");
+  console.log(toggleOpen);
+  toggleOpen.style.display = "none";
+  toggleClose.style.display = "block";
+};
 
+const toogleBtnClose = () => {
+  const ul = document.querySelector(".nav-list");
+  ul.style.display = "none";
+  const toggleOpen = document.querySelector("#toggle-open");
+  const toggleClose = document.querySelector("#toggle-close");
+  console.log(toggleOpen);
+  toggleOpen.style.display = "block";
+  toggleClose.style.display = "none";
 };
