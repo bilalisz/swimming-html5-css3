2d00e1b17942c553e02e2a8f585cebaadb6f0aaf
Bilal Khan <bilal.khan@argonteq.com>
2021-06-14 11:25:17 +0500

    swimming updated

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-const mediaQuery = window.matchMedia("( min-width: 768px )");
 
-// Note the `matches` property
-if (mediaQuery.matches) {
-  console.log("Media Query Matched!", "( min-width: 768px )");
-}
 
 const toogleBtn = () => {
   const ul = document.querySelector(".nav-list");
@@ -25,3 +20,5 @@ const toogleBtnClose = () => {
   toggleOpen.style.display = "block";
   toggleClose.style.display = "none";
 };
+
+
